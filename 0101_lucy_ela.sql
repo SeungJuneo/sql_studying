a63c27641b0cb3bbb6db76a9bd9d2c3404690908
Neajune <corea3june@naver.com>
2024-01-01 09:38:32 +0900

    feat: 주석 추가 - 0101_lucy_ela.sql 주석 추가

--- 0101_lucy_ela.sql
+++ 0101_lucy_ela.sql
@@ -1,1 +1,3 @@
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS WHERE NAME IN('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty') ORDER BY ANIMAL_ID;
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS WHERE NAME IN('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty') 
+-- ORDER BY 하라는 말이 없어도 해야할 때가 있다..
+ORDER BY ANIMAL_ID;
