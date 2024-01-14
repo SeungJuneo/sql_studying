85b91aa49fd6bcad36e42b55a704a20a52efbfdc
Neajune <corea3june@naver.com>
2024-01-14 17:08:06 +0900

    feat: 주석 추가 -j0114_Sosu.java 생성

--- j0114_Sosu.java
+++ j0114_Sosu.java
@@ -4,29 +4,30 @@ import java.util.HashSet;
 
 public class j0109_Sosu{
     public int solution(String numbers) {
-        HashSet<Integer> primeSet = new HashSet<>();
-        //??
-        generatePrimes("", numbers.toCharArray(), new boolean[numbers.length()], primeSet);
-        return primeSet.size();
+        HashSet<Integer> hashSet1 = new HashSet<>();
+        gp("", numbers.toCharArray(), new boolean[numbers.length()], hashSet1);
+        return hashSet1.size();
     }
-    //로직?
-    private void generatePrimes(String current, char[] numArray, boolean[] visited, HashSet<Integer> primeSet) {
+    //제곱근으로 소수 판단하고 재귀함수로 한 숫자씩 더해감.
+    private void gp(String current, char[] numArray, boolean[] visited, HashSet<Integer> hashSet1) {
         if (!current.isEmpty()) {
             int num = Integer.parseInt(current);
+            //hashSet1에 추가할지 말지 결정
             if (isPrime(num)) {
-                primeSet.add(num);
+                hashSet1.add(num);
             }
         }
-
+        //재귀함수
         for (int i = 0; i < numArray.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                generatePrimes(current + numArray[i], numArray, visited, primeSet);
+                gp(current + numArray[i], numArray, visited, hashSet1);
                 visited[i] = false;
             }
         }
     }
-//sqrt?
+//sqrt = 제곱근
+    //num에 제곱근만큼 반복해서 나눴을 경우 나머지가 0인지 판단하는 메서드
     private boolean isPrime(int num) {
         if (num < 2) {
             return false;
